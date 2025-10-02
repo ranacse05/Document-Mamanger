ec85f2901414c60b19af5517e563cdc016139019
ranacse05 <ranacse05@gmail.com>
2025-10-02 20:36:47 +0600

    API endpoints added

--- app/api/documents/[id]/route.ts
+++ app/api/documents/[id]/route.ts
@@ -16,3 +16,16 @@ export async function PUT(request: any, { params }: { params: { id: string } }) 
     return NextResponse.json({ message: "An error occurred." }, { status: 500 });
   }
 }
+
+
+export async function GET(request: any, { params }: { params: { id: string } }) {
+  const { id } = params;
+  try {
+    await connectMongoDB();
+    const Details = await Document.findOne({ _id: id});
+    return NextResponse.json({ Details }, { status: 200 });
+  } catch (error) {
+    console.error("API Route Error:", error);
+    return NextResponse.json({ message: "An error occurred." }, { status: 500 });
+  }
+}
